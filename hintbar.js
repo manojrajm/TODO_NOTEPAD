86fc13ea64c025f6a6dc54c41aab116da74d5511
manojrajm <gauthamtamziha007@gmail.com>
2022-07-28 20:17:51 +0530

    changes in typebar

--- hintbar.js
+++ hintbar.js
@@ -1,94 +1,80 @@
-const box = document.querySelector('.box');
-const text = document.querySelector('.text');
-const save = document.querySelector('.save');
-const view = document.querySelector('.view');
-const del = document.querySelector('.del');
-const togglebtn = document.getElementsByClassName('togglebtn');
-const input = document.getElementById('input-el');
-const cntnt = document.querySelector('.cntnt')
-// togglebtn.onclick=function(){
-//     box.classList.toggle('active');
-//
-// togglebtn.addEventListener("click", (e) => {
-//   console.log("mmm")
-//   });
+const box = document.querySelector(".box");
+const text = document.querySelector(".text");
+const save = document.querySelector(".save");
+const view = document.querySelector(".view");
+const del = document.querySelector(".del");
+const togglebtn = document.getElementsByClassName("togglebtn");
+const input = document.getElementById("input-el");
+const cntnt = document.querySelector(".cntnt");
 
-const toggle = document.getElementById('toggle');
 
-toggle.addEventListener("click",()=>{
-  box.classList.toggle('active');
-  text.classList.toggle('active');
-  save.classList.toggle('active');
-  view.classList.toggle('active'); 
-  del.classList.toggle('active');
+const toggle = document.getElementById("toggle");
+
+toggle.addEventListener("click", () => {
+  box.classList.toggle("active");
+  text.classList.toggle("active");
+  save.classList.toggle("active");
+  view.classList.toggle("active");
+  del.classList.toggle("active");
   if (cntnt.style.display === "grid") {
     cntnt.style.display = "none";
   } else {
     cntnt.style.display = "grid";
   }
-  
-  // box.classList.toggle('box');
-})
 
+  
+});
 
 let elements = [];
 save.addEventListener("click", () => {
   if (input.value.length > 0) {
-      elements.push(input.value);
-      input.value = '';
-      console.log(elements);
-      localStorage.setItem("elements", JSON.stringify(elements));
-      console.log(elements);
+    elements.push(input.value);
+    input.value = "";
+
+    localStorage.setItem("elements", JSON.stringify(elements));
+    console.log(elements);
   } else {
-      alert("notes is empty please enter the notes")
+    alert("notes is empty please enter the notes");
   }
-})
+});
 
 function getData() {
-  return JSON.parse(localStorage.getItem('elements') || "[]");
+  return JSON.parse(localStorage.getItem("elements") || "[]");
 }
 
 view.addEventListener("click", () => {
-  // let elements = '';
-  var list = document.getElementById('ul-el');
-  // const data = getData(); 
-  list.innerHTML =elements;
-  cntnt.style.display="grid";
-})
+ 
+  var list = document.getElementById("ul-el");
+ 
+  list.innerHTML = elements;
+  cntnt.style.display = "grid";
+});
 
 function getData() {
-  return JSON.parse(localStorage.getItem('elements') || "[]");
+  return JSON.parse(localStorage.getItem("elements") || "[]");
 }
 
-view.addEventListener('click', () => {
-
+view.addEventListener("click", () => {
   let currentURL = elements;
 
-  elements.push(currentURL)
+  elements.push(currentURL);
 
-  // deleteValue();
-})
-
-
-del.addEventListener("click",()=>{
-  let shift = elements.pop(elements)
-  
-  for (let i = 0; i < elements.length; i++) {
-         let shift = elements.splice(i, elements.length);
-  
-   console.log(shift);
-  }
-})
-// function deleteVal(){
-//   for (let i = 0; i < elements.length; i++) {
-//     var shift = elements.splice(i, elements.length);
-//     console.log(shift);
-// }
-// }
-window.onload = function () {
-  var list = document.getElementById('ul-el');
-  const data = getData();
-  elements = JSON.parse(localStorage.getItem("elements")) || []; 
-  list.innerHTML=data;
-}
  
+});
+
+del.addEventListener("click", () => {
+  let shift = elements.pop(elements);
+
+  for (let i = 0; i <= elements.length; i++) {
+    let shift = elements.splice(i, elements.length);
+
+    console.log(shift);
+  }
+});
+
+window.onload = function () {
+  var list = document.getElementById("ul-el");
+  const data = getData();
+  elements = JSON.parse(localStorage.getItem("elements")) || [];
+  list.innerHTML = data;
+};
